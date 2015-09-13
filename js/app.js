ddfe9d9b55201815cc0268adbfc3360b509db05d
Yaz Khoury <yaz.khoury@gmail.com>
2015-09-13 16:25:54 -0400

    feat: Make yPosition function, add comments, fix indentation

--- js/app.js
+++ js/app.js
@@ -1,156 +1,171 @@
+/*
+	Author: Yaz Khoury
+	Title: Frogger Clone Game
+	Date: September 2015
+*/
+
+//
+// Global Variables
+//
+
 var arcadeWidth = 400;
 var arcadeHeight = 400;
 var liveNumber = 5;
 var scoreKeeper = 0;
 
-// Enemies our player must avoid
+
+//
+//  Enemy Class
+//
+
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+	// The image/sprite for our enemies
+	this.sprite = 'images/enemy-bug.png';
 
-    // var yPosition = function() {
-    //     var y = [50, 150, 225];
-    //     return y[Math.floor(Math.random() * y.length)];
-    // };
+	// Function for randomizing enemy initial position
+	var yPosition = function() {
+		var y = [50, 150, 225];
+		return y[Math.floor(Math.random() * y.length)];
+	};
 
+	// Enemy initial coordinates
+	this.x = -100;
+	this.y = yPosition();
 
-    // Enemy coordinates
-    this.x = -100;
-    this.yPosition = [50, 150, 225];
+	// Enemy height and width
+	this.height = 50;
+	this.width = 50;
 
-    this.height = 50;
-    this.width = 50;
+	// Function for random speed generator
+	function getRandomSpeed(min, max) {
+		return Math.floor(Math.random() * max) + min;
+	};
 
-
-    function getRandomSpeed(min, max) {
-        return Math.floor(Math.random() * max) + min;
-    };
-
-    this.speed = getRandomSpeed(80,200);
-
-    // this.speed = Math.floor(Math.random() * (300 - 200)) + 200;
-
+	this.speed = getRandomSpeed(80,200);
 
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Update the enemy's position, detect collision, and resetting
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x += this.speed * dt;
 
-    //COLLISION
-    if (this.x < player.x + player.width &&
-        this.x + this.width > player.x &&
-        this.y < player.y + player.height &&
-        this.height + this.y > player.y) {
-        player.reset();
-        liveNumber--;
-    }
+	// Multiply movement by dt parameter, ensuring game runs the same speed on all computers.
+	this.x += this.speed * dt;
 
-    // RESET AFTER reaching edge
-    if (this.x > arcadeWidth + 100){
-        this.reset();
-    }
+	// Collision detection
+	if (this.x < player.x + player.width &&
+		this.x + this.width > player.x &&
+		this.y < player.y + player.height &&
+		this.height + this.y > player.y) {
+		player.reset();
+		liveNumber--;
+	}
+
+	// Reseting after reaching edge
+	if (this.x > arcadeWidth + 100){
+		this.reset();
+	}
 
 };
 
-// Draw the enemy on the screen, required method for game
+// Rendering the enemy on the screen
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Resetting enemy position after reaching the edge
 Enemy.prototype.reset = function() {
-    this.x = -100;
-    this.y = this.yPosition[Math.floor(Math.random() * this.yPosition.length)];
+	this.x = -100;
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
+//
+//  Player Class
+//
+
 var Player = function() {
-    this.x = 202;
-    this.y = 350;
-    this.height = 50;
-    this.width = 50;
-    this.speed = 25;
-    this.sprite = "images/char-boy.png";
+	// Player initial coordinates
+	this.x = 202;
+	this.y = 350;
+	// Player width and height
+	this.height = 50;
+	this.width = 50;
+	// Player Speed
+	this.speed = 25;
+	// Player sprite
+	this.sprite = 'images/char-boy.png';
 };
 
+// Live number and score counting
 Player.prototype.update = function(){
-    if (this.y <= 3){
-        this.reset();
-        scoreKeeper++;
-    }
 
-    if (liveNumber < 1){
-        alert('Game Over');
-        liveNumber = 5;
-        scoreKeeper = 0;
-    }
+	if (this.y <= 3){
+		this.reset();
+		scoreKeeper++;
+	}
+
+	if (liveNumber < 1){
+		console.log('Game Over');
+		liveNumber = 5;
+		scoreKeeper = 0;
+	}
 };
 
+// Render player image, along with scores and lives
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
-    ctx.font = '25px Lato';
-    ctx.textAlign = 'right';
-    ctx.fillStyle= 'white';
+	ctx.font = '25px Lato';
+	ctx.textAlign = 'right';
+	ctx.fillStyle= 'white';
 
-    //Score Counter Text
-    ctx.fillText('Score:', 85, 85);
-    ctx.fillText(scoreKeeper, 60, 115);
+	//Score Counter Text
+	ctx.fillText('Score:', 85, 85);
+	ctx.fillText(scoreKeeper, 60, 115);
 
-    //Lives Counter Text
-    ctx.fillText('Lives:', 485, 85);
-    ctx.fillText(liveNumber, 460, 115);
+	//Lives Counter Text
+	ctx.fillText('Lives:', 485, 85);
+	ctx.fillText(liveNumber, 460, 115);
 }
 
+// Reset player position
 Player.prototype.reset = function(){
-    this.x = 200;
-    this.y = 400;
+	this.x = 200;
+	this.y = 400;
 }
 
-//advaces player according to key input
+// Move player according to key input
 Player.prototype.handleInput = function(key) {
-    switch (key) {
-        case 'up':
-            this.y = this.y - this.speed;
-            break;
-        case 'down':
-            if (this.y < arcadeHeight) {
-                this.y = this.y + this.speed;
-            }
-            break;
-        case 'left':
-            if (this.x > 0){
-                this.x = this.x - this.speed;
-            }
-            break;
-        case 'right':
-            if (this.x < arcadeWidth){
-            this.x = this.x + this.speed;
-            }
-            break;
-        default:
-            console.log('Please select an arrow key')
-    }
+	switch (key) {
+		case 'up':
+			this.y = this.y - this.speed;
+			break;
+		case 'down':
+			if (this.y < arcadeHeight) {
+				this.y = this.y + this.speed;
+			}
+			break;
+		case 'left':
+			if (this.x > 0){
+				this.x = this.x - this.speed;
+			}
+			break;
+		case 'right':
+			if (this.x < arcadeWidth){
+			this.x = this.x + this.speed;
+			}
+			break;
+		default:
+			console.log('Play with your arrow keys!')
+	}
 }
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Instantiate all objects
 var allEnemies = [
-    enemy = new Enemy(),
-    enemy2 = new Enemy(),
-    enemy3 = new Enemy()
+	enemy = new Enemy(),
+	enemy2 = new Enemy(),
+	enemy3 = new Enemy()
 ];
 
 var player = new Player();
@@ -159,12 +174,12 @@ var player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
 
-    player.handleInput(allowedKeys[e.keyCode]);
+	player.handleInput(allowedKeys[e.keyCode]);
 });
